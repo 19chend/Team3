dd0f03b2381d8deaa2fd407706c682f41125f4d1
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-12 13:54:16 -0600

    ahmedAverageInput

---
+++ src/ahmedAverageInput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ahmedAverageInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Give me a double value");
+		int num = input.nextInt();
+		int sum = 0;
+		int times = 0;
+
+		while (num != 9999) {
+			sum = sum+num;
+			times++;
+			System.out.println("Give me a double value");
+			 num = input.nextInt();
+		}
+
+		System.out.println("the average of the numbers is " + sum/times );
+	}
+
+}
